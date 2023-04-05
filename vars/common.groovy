4d12e2fa9f061a49bf8ffadcf7c0c3510db491c4
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-05 22:34:18 +0530

    for shipping java sonar_extra opts

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,9 @@
 def compile() {
-    if(app_lang == "nodejs") {
+    if (app_lang == "nodejs") {
         sh 'npm install'
     }
 
-    if(app_lang == "maven") {
+    if (app_lang == "maven") {
         sh 'mvn package'
     }
 
