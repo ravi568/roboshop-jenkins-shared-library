bae1e77fbfc72cf03bf6c07722c4db172aaee921
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-05 19:51:33 +0530

    all app components build and test using def call as global libraries with ci

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,7 +7,7 @@ def call() {
             stage('compile/build') {
                 steps {
                     script{
-                          common.compile
+                          common.compile()
                     }
 
                 }
