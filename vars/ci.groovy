5700135df7956ce8b659e79b80cdc18e0b834224
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-11 19:15:34 +0530

    no  main branch

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,16 +1,14 @@
 
 
-
-
-//    if (env.TAG_NAME ==~ ".*"){
-//        env.GTAG = "true"
-//    } else {
-//        env.GTAG = "false"
-//    }
-
 def call() {
     if (!env.sonar_extra_opts){
         env.sonar_extra_opts= ""
+    }
+
+    if (env.TAG_NAME ==~ ".*"){
+        env.GTAG = "true"
+    } else {
+        env.GTAG = "false"
     }
 
     node('workstation') {
@@ -18,51 +16,51 @@ def call() {
 
         try {
 
-                stage('check out code ') {
-                    cleanWs()
-                    git branch: 'main', url: 'https://github.com/ravi568/cart'
+            stage('check out code ') {
+                cleanWs()
+                git branch: 'main', url: 'https://github.com/ravi568/cart'
+            }
+
+            sh 'env'
+
+            if (env.BRANCH_NAME != "main") {
+                stage('compile/build') {
+                    common.compile()
+                }
+            }
+
+            println GTAG
+            println BRANCH_NAME
+
+            if (env.GTAG != "true" && env.BRANCH_NAME != "main") {
+                stage('test cases') {
+                    common.testcases()
+                }
+            }
+
+            if (BRANCH_NAME ==~ "PR-.*") {
+                stage('code quality') {
+                    common.codequality()
+                }
+            }
+
+
+            if (env.GTAG == "true") {
+                stage('package') {
+                    common.testcases()
                 }
 
-                sh 'env'
-
-                if (env.BRANCH_NAME != "main") {
-                    stage('compile/build') {
-                        common.compile()
-                    }
+                stage('Artifact Upload') {
+                    common.testcases()
                 }
+            }
 
-        } catch (e) {
+        }  catch (e) {
             mail body: "<h1>${component} - pipeline failed \n ${BUILD_URL}</h1>", from: 'ravidevopsprasad@gmail.com', subject: "${component} - pipeline failed", to: ' ravidevopsprasad@gmail.com', mimeType: 'text/html'
 
         }
     }
 
-}
-
-    //         println GTAG
-    //         println BRANCH_NAME
-    //
-    //       if (env.GTAG != "true" && env.BRANCH_NAME != "main") {
-    //           stage('test cases') {
-    //               common.testcases()
-    //           }
-    //       }
-
-    //       if (BRANCH_NAME ==~ "PR-.*") {
-    //           stage('code quality') {
-    //               common.codequality()
-    //           }
-    //       }
-    //
-    //
-    //         if (env.GTAG == "true") {
-    //             stage('package') {
-    //                 common.testcases()
-    //             }
-    //
-    //             stage('Artifact Upload') {
-    //                 common.testcases()
-    //             }
-
+    }
 
 
