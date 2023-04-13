2c532dc6cf06cabae6c56a8bf125c5829d8d1cd2
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-04-13 17:21:30 +0530

    username and password mask

--- vars/common.groovy
+++ vars/common.groovy
@@ -37,9 +37,12 @@ def prepareArtifacts(){
 
 
 def Artifactupload(){
-    sh 'echo ${TAG_NAME} >VERSION'
-    //if (app_lang == "nodejs" || app_lang == "angular") {
-        sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.13.128:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-    //}
+    env.NEXUS_USER = sh ( script: 'aws ssm get-parameter --name prod.nexus.user --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    env.NEXUS_PASS = sh ( script: 'aws ssm get-parameter --name prod.nexus.pass --with-decryption | jq .Parameter.Value | xargs', returnStdout: true ).trim()
+    wrap([$class: 'MaskPasswordsBuildWrapper',
+          varPasswordPairs: [[password: NEXUS_PASS],[password: NEXUS_USER]]]) {
+        sh 'echo ${TAG_NAME} >VERSION'
+        sh 'curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.13.128:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+    }
 
 }
