cbaee17ed87318dbb7032f1c705e9db781574516
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-05-09 10:26:39 +0530

    helm deploy job, jenkinsfile for helm, pod yaml in helm and prod input yaml for component

---
+++ vars/helm.groovy
@@ -0,0 +1,41 @@
+def call() {
+    pipeline {
+        agent any
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            string(name: 'app_version', defaultValue: '', description: 'App Version')
+            string(name: 'component', defaultValue: '', description: 'Component')
+            string(name: 'environment', defaultValue: '', description: 'Environment')
+        }
+
+        stages {
+            stage('Clone Application') {
+                steps {
+                    dir('APP'){
+                        git branch: 'main', url: "https://github.com/ravi568/${component}"
+                    }
+
+                }
+            }
+            stage('Deploy Helm Chart') {
+                steps {
+                    script {
+                        sh 'helm install ${component} APP/helm/${environment.yaml}'
+                        }
+                    }
+
+                }
+            }
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
