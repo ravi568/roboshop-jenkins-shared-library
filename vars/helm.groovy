0f65a7f19f91b3b8806337e3a6d30529871df5f3
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-05-10 13:37:08 +0530

    helm component for cart and catalogue all files updated including helm groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -24,7 +24,7 @@ def call() {
             stage('Deploy Helm Chart') {
                 steps {
                     script {
-                        sh 'helm install ${component} . -f APP/helm/${environment}.yaml --set appversion=${app_version}'
+                        sh 'helm upgrade -i ${component} . -f APP/helm/${environment}.yaml --set appversion=${app_version}'
                         }
                     }
 
